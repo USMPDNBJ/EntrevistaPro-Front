a03844bfce80df8710d1f36d34eeebb732243418
JC-Morales03 <juan_morales8@usmp.pe>
2025-05-09 17:07:43 -0500

    About Us

--- src/app/Views/Principal/about-us/about-us.component.ts
+++ src/app/Views/Principal/about-us/about-us.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from '../../../Components/footer/footer.component';
+import { NavbarComponent } from '../../../Components/navbar/navbar.component';
 
 @Component({
   selector: 'app-about-us',
-  imports: [],
+  imports: [FooterComponent, NavbarComponent],
   standalone: true,
   templateUrl: './about-us.component.html',
   styleUrl: './about-us.component.css'
