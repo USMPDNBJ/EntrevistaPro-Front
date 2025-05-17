ed827a157aa1cdf43a5a439c132c9ebcbee9aa3b
USMPDNBJ <david_nolasco@usmp.pe>
2025-05-16 23:13:19 -0500

    tabla sesiones por usuario

--- src/app/models/sessions.ts
+++ src/app/models/sessions.ts
@@ -1,11 +1,11 @@
 export default interface Session {
-  id: string;           // Identificador único de la sesión
-  usuario_id: string;   // Identificador del usuario asociado
-  fecha: Date;        // Fecha de la sesión
-  hora_inicio: string;  // Hora de inicio de la sesión
-  hora_fin: string;     // Hora de fin de la sesión
-  estado: string;       // Estado de la sesión (por ejemplo: "activa", "finalizada")
-  evaluacion: string;   // Evaluación de la sesión (si aplica)
-  creado_en: Date;    // Fecha en la que se creó la sesión
-  enlace: string;       // Enlace asociado con la sesión (si aplica)
+  id: number;
+  trabajador: string;  // Nombre del trabajador
+  fecha: Date;      // Fecha en formato ISO 8601
+  hora_inicio: string; // Hora de inicio de la sesión
+  hora_fin: string;    // Hora de fin de la sesión
+  estado: string;      // Estado de la sesión
+  evaluacion: string;  // URL del archivo PDF de evaluación
+  creado_en: string;   // Fecha de creación
+  enlace: string;
 }
