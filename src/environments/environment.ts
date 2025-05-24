1a2d5d00226c4b5e0e968d02de494115a4adff7f
USMPDNBJ <david_nolasco@usmp.pe>
2025-05-23 23:02:30 -0500

    correcion

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:5001/api/user'
-};
-
-export const environmentSession = {
-  production: false,
-  apiUrl: 'http://localhost:5001/api/session'
+  apiUrlUser: 'http://localhost:5001/api/user',
+  apiUrlSession: 'http://localhost:5001/api/session',
+  apiUrlPago: 'http://localhost:5001/api/pago'
 };
